6cf14af9eceadb306489675c3a97574e1334cf19
1F10240261 <s1f102402619@iniad.org>
2025-07-10 22:15:35 +0900

    buildを修正

--- build.sh
+++ build.sh
@@ -1,12 +1,7 @@
-#!/url/bin/env bash
-echo "1"
+#!/usr/bin/env bash
 set -o errexit
-echo "2"
 
 pip install -r requirements.txt
-echo "3"
 
 python manage.py collectstatic --no-input
-echo "4"
 python manage.py migrate
-echo "5"
